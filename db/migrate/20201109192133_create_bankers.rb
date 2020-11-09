fe6986b31c1e491844eee4d162fac92eb42dbb3a
adnapp <adam.napps@gmail.com>
2020-11-09 15:01:01 -0500

    Add banker,game,user models

---
+++ db/migrate/20201109192133_create_bankers.rb
@@ -0,0 +1,9 @@
+class CreateBankers < ActiveRecord::Migration[5.2]
+  def change
+    create_table :bankers do |t| 
+      t.string :name
+      t.integer :commission_rate
+
+   end
+  end
+end
---
+++ db/migrate/20201109192138_create_games.rb
@@ -0,0 +1,15 @@
+class CreateGames < ActiveRecord::Migration[5.2]
+  def change
+
+    create_table :games do |t| 
+      t.string :user_id
+      t.string :banker_id
+      t.integer :wager
+      t.integer :player_hand
+      t.integer :banker_hand
+      t.integer :player_third_card
+
+
+   end
+  end
+end
