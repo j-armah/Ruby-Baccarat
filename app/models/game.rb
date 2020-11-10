b8c67174b488d7e762803a5327707957a8ca26cd
februar-y <jarmah241@gmail.com>
2020-11-09 19:41:58 -0500

    add values to each card in deck, create player hand

--- app/models/game.rb
+++ app/models/game.rb
@@ -5,34 +5,73 @@ class Game < ActiveRecord::Base
     belongs_to :banker
     belongs_to :user
 
+    def value=(value)
+        @value = value
+    end
+
     def self.deckofcards
-        deck = DeckOfCards.new
-        deck.shuffle
+        DeckOfCards.new.shuffle
+    end
+
+    def self.value_cards
+        new_deck = []
+        deck = deckofcards.map{|card| [card.to_s, value:card.value]}
+        deck.map do |key, value|
+            if key.include?("Jack") || key.include?("Queen") || key.include?("King") || key.include?("10")
+                value[:value] = 0
+            elsif key.include?("Ace")
+                value[:value] = 1
+            end
+        end
         deck
     end
 
+    # def self.card_revalue
+    #     self.deckofcards.cards.each do |card|
+    #         if card.rank == "Jack" || card.rank == "Queen" || card.rank == "King" || card.rank == 10
+    #             card.value = 0
+    #         end
+    #     end
+    # end
+
     def self.draw_card
-        self.deckofcards.draw
+        self.value_cards.sample
     end
 
     def self.player_hand
-        card1 = self.draw_card
-        card2 = self.draw_card
-        [card1, card2]
+        puts "The player draws two cards."
+        sleep(1)
+        user_card1 = self.draw_card
+        user_card2 = self.draw_card
+        user_card1_value = user_card1[1][:value]
+        user_card2_value = user_card2[1][:value]
+
+        puts "Player's Hand: #{user_card1[0]} #{user_card2[0]}"
+        sleep(1)
+
+        player_hand_value = user_card1_value + user_card2_value
+        if player_hand_value <= 5 
+            puts "The player hand value is #{player_hand_value}, draw again"
+            user_card3 = self.draw_card
+            user_card3_value = user_card3[1][:value]
+            puts "The player draws: #{user_card3[0]}"
+            player_hand_value += user_card3_value
+            puts "The player hand value is now #{player_hand_value}"
+        elsif player_hand_value > 9
+            player_hand_value -= 10
+            puts "Player hand value over 10, subtract 10"
+            puts "Player hand value is now #{player_hand_value}"
+        else
+            puts "The player will stay, hand value is: #{player_hand_value}"
+        end
     end
 
     def self.banker_hand
-        card1 = self.draw_card
-        card2 = self.draw_card
-        [card1, card2]
+        banker_card1 = self.draw_card
+        banker_card2 = self.draw_card
+        banker_card1_value = banker_card1[1][:value]
+        banker_card2_value = banker_card2[1][:value]
     end
-
-
-
-
-    # Deck of cards
-    # change card values
-    # shuffle
 
 
     # Banker Hand holds value
--- bin/run.rb
+++ bin/run.rb
@@ -4,10 +4,23 @@ require 'deck-of-cards'
 
 #binding.pry
 
+Game.player_hand
+
+
+
+
+
+
+
+
+
+
+#Game.card_revalue
+
 # deck = DeckOfCards.new
 # deck.shuffle
 # p deck
-p Game.player_hand
+#p Game.player_hand
 # deck = Game.deckofcards
 #deck.shuffle
-binding.pry
+#binding.pry
